52ec14c4969ba826cc2700b433f46e0fc054955e
S.Samarsky <js.samarsky@gmail.com>
2023-02-07 21:54:18 +0300

    creat base routing

--- app.js
+++ app.js
@@ -1,24 +1,49 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   console.log("Server request");
-  console.log(req.url, req.method);
 
-  res.setHeader('Content-Type', 'application/json')
+  res.setHeader("Content-Type", "text/html");
 
-  // res.setHeader('Content-Type', 'text/html')
-  // res.write('<head><link rel="stylesheet" href="#"/></head>')
-  // res.write('<h1>Hello World</h1>');
-  // res.write('<p>Like this is cool!</p>');
+  const creatPath = (page) => path.resolve(__dirname, "views", `${page}.html`);
 
-  const data = JSON.stringify([
-    { name: "Tommy", age: 30 },
-    { name: "Joy", age: 25 },
-  ]);
+  let basePath = "";
+  switch (req.url) {
+    case "/":
+    case "/home":
+    case "/index.html":
+      basePath = creatPath("index");
+      res.statusCode = 200;
+      break;
+    case "/about":
+      res.statusCode = 301;
+      res.setHeader("Location", "/contacts");
+      res.end();
+      break;
+    case "/contacts":
+      basePath = creatPath("contacts");
+      res.statusCode = 200;
+      break;
+    default:
+      basePath = creatPath("error");
+      res.statusCode = 404;
+      break;
+  }
 
-  res.end(data);
+  fs.readFile(basePath, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.statusCode = 500;
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
 server.listen(PORT, "localhost", (error) => {
