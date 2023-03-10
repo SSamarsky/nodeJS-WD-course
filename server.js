8dcc7a6ed715cab4c7d5a9e3219e85da173ea90a
S.Samarsky <js.samarsky@gmail.com>
2023-03-10 17:48:01 +0300

    refactor: refactoring code (data)

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
@@ -8,23 +8,21 @@ const creatPath = (page) => path.resolve(__dirname, "views", `${page}.html`);
 const PORT = 3000;
 
 app.listen(PORT, (error) => {
-    error ? console.log(error) : console.log(`listening port ${PORT}`);
+  error ? console.log(error) : console.log(`listening port ${PORT}`);
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(creatPath('index'));
+app.get("/", (req, res) => {
+  res.sendFile(creatPath("index"));
 });
 
-app.get('/contacts', (req, res) => {
-    res.sendFile(creatPath('contacts'));
+app.get("/contacts", (req, res) => {
+  res.sendFile(creatPath("contacts"));
 });
 
-app.get('/about', (req, res) => {
-    res.redirect('/contacts');
+app.get("/about", (req, res) => {
+  res.redirect("/contacts");
 });
 
 app.use((req, res) => {
-    res
-    .status(404)
-    .sendFile(creatPath('error'))
-})
+  res.status(404).sendFile(creatPath("error"));
+});
